0c7fc245e818047f927e7bb351d122e80908088f
Flavio Repos <buzonplataforma12@gmail.com>
2024-04-27 11:19:35 -0600

    fine tone with the modal in HTML and button listener and first fuctions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
 // make references to the HTML
 const addTaskButtonEl = document.getElementById("addTaskButton");
+const otroButton = $('#addTaskButton');
+const inputTaskTitle = document.getElementById("inputTitle");
+const otroInput = $('#inputTitle');
+
 
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+// let taskList = JSON.parse(localStorage.getItem("tasks"));
+// let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+// An array for the tasks
+let ArrayofTasks = [
+    { id:String,
+      title:String
+    }
+];
+
+// An array to receive the inputs of the form
+ let oneTaskArray = [
+    { id:String,
+      title:String
+    }
+];
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -12,6 +30,9 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    //const taskCard = $('<div>')
+    //.addClass('card task-card draggable my-3');
+    // .attr('data-task-id', project.id);
 
 }
 
@@ -24,8 +45,21 @@ function renderTaskList() {
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
     console.log('In handleaddtask function');
+    console.log(inputTaskTitle.value);
+    console.log(otroInput.val().trim());
+    console.log(otroButton);
+    oneTaskArray = {
+        "title": String(inputTaskTitle.value),
+      };
+      // adding a row to the array of objets 
+      ArrayofTasks.push(oneTaskArray);
+      // push to the local storage
+      localStorage.setItem('tasks',JSON.stringify(ArrayofTasks));
+
+    console.log('end of handleaddtask function');
 
 }
+
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
